46ac02417705fd603a4e95a5fcb4074b3b23d128
Artur20 <artyrfzf11@gmail.com>
2021-01-27 22:45:06 +0200

    Completed Task 2

--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,8 @@
-import nookies from 'nookies';
-import fs from 'fs';
+import {withUser} from 'utils/withUser';
 
-import {renderId} from 'helpers/renderId';
+export const getServerSideProps = withUser();
 
-export const getServerSideProps = async (ctx) => {
-  const userId = +nookies.get(ctx).userId || renderId();
-  const getFile = new Promise((resolve) => {
-    fs.readFile("users.json", {encoding: 'utf-8'}, (err, data) => {
-      resolve(err ? [] : JSON.parse(data))
-    });
-  });
-  const file = await getFile;
-  const visitCounts = file.find((user) => user.userId === userId)?.visitCounts + 1 || 1;
-
-  nookies.set(ctx, 'userId', userId);
-  fs.writeFile(
-    "users.json",
-    JSON.stringify(visitCounts === 1 ?
-      [...file, {userId, visitCounts}]
-        : file.map(user => user.userId === userId ? {userId, visitCounts} : user)),
-    () => {}
-  )
-
-  return {
-    props: {
-      isGuest: visitCounts < 3,
-      isFriend: visitCounts >= 3 && visitCounts < 5,
-      isFamily: visitCounts >= 5
-    }
-  }
-}
-
-export default function Home({isGuest, isFriend, isFamily}) {
+const Home = ({isGuest, isFriend, isFamily}) => {
   const guestJSX = isGuest && (
     <h1>Приветствуем тебя странник!</h1>
   )
@@ -52,3 +23,5 @@ export default function Home({isGuest, isFriend, isFamily}) {
     </div>
   )
 }
+
+export default Home;
