ec3d3220764bd1eeb27cc628a91829bc52c7fa12
Artur20 <artyrfzf11@gmail.com>
2021-02-01 19:47:28 +0200

    Fixed Tasks 1, 2

--- pages/user.js
+++ pages/user.js
@@ -1,18 +1,20 @@
 import {initialDispatcher} from "init/initialDispatcher";
 import {initializeStore} from "init/store";
+import {userActions} from "bus/user/actions";
 import User from "components/User";
 import BaseLayout from "components/layouts/BaseLayout";
+import {withUser} from "utils/withUser";
 
-export const getServerSideProps = async (ctx) => {
+export const getServerSideProps = withUser(async (ctx, user) => {
   const store = await initialDispatcher(ctx, initializeStore());
+  store.dispatch(userActions.fillUser(user));
+
   const initialReduxState = store.getState();
 
-  return  {
-    props: {
-      initialReduxState
-    }
+  return {
+    initialReduxState
   }
-}
+});
 
 const UserPage = () => {
   return (
