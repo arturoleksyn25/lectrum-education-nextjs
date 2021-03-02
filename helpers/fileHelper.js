47f377ac461b035b6abce68182a24fcaac91e69a
Artur20 <artyrfzf11@gmail.com>
2021-03-02 20:05:28 +0200

    Fixed path

--- helpers/fileHelper.js
+++ helpers/fileHelper.js
@@ -4,7 +4,7 @@ import {reject} from "ramda";
 
 export const getFile = (url) => {
   return new Promise((resolve) => {
-    fs.readFile(path.resolve(url), {encoding: 'utf-8'}, (err, data) => {
+    fs.readFile(path.resolve(`/tmp/${url}`), {encoding: 'utf-8'}, (err, data) => {
       resolve(err ? [] : JSON.parse(data))
     });
   });
@@ -12,7 +12,7 @@ export const getFile = (url) => {
 
 export const setFile = (url, data) => {
   return new Promise((resolve) => {
-    fs.writeFile(path.resolve(url), JSON.stringify(data), (err) => {
+    fs.writeFile(path.resolve(`/tmp/${url}`), JSON.stringify(data), (err) => {
       if (err) {
         reject(err)
         throw err;
